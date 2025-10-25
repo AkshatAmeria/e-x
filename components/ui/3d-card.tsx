9a42371981b943e09d506da3a9dfe6d7a67d0119
Akshat Ameria <1201akshat@gmail.com>
2025-10-25 14:29:45 +0530

    Update 3d.tsx error files only

--- components/ui/3d-card.tsx
+++ components/ui/3d-card.tsx
@@ -95,8 +95,61 @@ export const CardBody = ({
   );
 };
 
-export const CardItem = ({
-  as: Tag = "div",
+// export const CardItem = ({
+//   as: Tag = "div",
+//   children,
+//   className,
+//   translateX = 0,
+//   translateY = 0,
+//   translateZ = 0,
+//   rotateX = 0,
+//   rotateY = 0,
+//   rotateZ = 0,
+//   ...rest
+// }: {
+//   as?: React.ElementType;
+//   children: React.ReactNode;
+//   className?: string;
+//   translateX?: number | string;
+//   translateY?: number | string;
+//   translateZ?: number | string;
+//   rotateX?: number | string;
+//   rotateY?: number | string;
+//   rotateZ?: number | string;
+//   [key: string]: any;
+// }) => {
+//   const ref = useRef<HTMLDivElement>(null);
+//   const [isMouseEntered] = useMouseEnter();
+
+//   useEffect(() => {
+//     handleAnimations();
+//   }, [isMouseEntered]);
+
+//   const handleAnimations = () => {
+//     if (!ref.current) return;
+//     if (isMouseEntered) {
+//       ref.current.style.transform = `translateX(${translateX}px) translateY(${translateY}px) translateZ(${translateZ}px) rotateX(${rotateX}deg) rotateY(${rotateY}deg) rotateZ(${rotateZ}deg)`;
+//     } else {
+//       ref.current.style.transform = `translateX(0px) translateY(0px) translateZ(0px) rotateX(0deg) rotateY(0deg) rotateZ(0deg)`;
+//     }
+//   };
+
+//   return (
+   
+//     <Tag
+//       ref={ref}
+//       className={cn("w-fit transition duration-200 ease-linear", className)}
+//       {...rest}
+//     >
+      
+//       {children}
+//     </Tag>
+//   );
+// };
+
+
+export const CardItem = <T extends React.ElementType = "div">({
+  as,
   children,
   className,
   translateX = 0,
@@ -107,7 +160,7 @@ export const CardItem = ({
   rotateZ = 0,
   ...rest
 }: {
-  as?: React.ElementType;
+  as?: T;
   children: React.ReactNode;
   className?: string;
   translateX?: number | string;
@@ -116,8 +169,8 @@ export const CardItem = ({
   rotateX?: number | string;
   rotateY?: number | string;
   rotateZ?: number | string;
-  [key: string]: any;
-}) => {
+} & React.ComponentPropsWithoutRef<T>) => {
+  const Tag = as || "div";
   const ref = useRef<HTMLDivElement>(null);
   const [isMouseEntered] = useMouseEnter();
 
@@ -135,17 +188,17 @@ export const CardItem = ({
   };
 
   return (
-   
-    <Tag
+    //@ts-ignore
+    <Tag 
       ref={ref}
       className={cn("w-fit transition duration-200 ease-linear", className)}
       {...rest}
     >
-      
       {children}
     </Tag>
   );
 };
+
 
 // Create a hook to use the context
 export const useMouseEnter = () => {
